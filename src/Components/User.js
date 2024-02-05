29fec8a755822b7f423e063b3ca0fca4ecbc3e2d
vvishnu17 <vishnuramadass17@gmail.com>
2024-02-05 11:29:55 +0100

    add life cycle method Unmount

--- src/Components/User.js
+++ src/Components/User.js
@@ -2,6 +2,9 @@ import React, {Component} from "react";
 import styles from './User.module.css';
 
 class User extends Component{
+    componentWillUnmount(){
+        console.log("User Component is unmounted");
+    }
     render() {
         return <li className={styles.user}>
             {this.props.name}
